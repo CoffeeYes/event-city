0549a536187427dc2ccb8b92f004b6cc798feeba
CoffeeYes <culinecro@gmail.com>
2018-04-23 19:40:19 +0200

    removed default html

--- src/views/App.js
+++ src/views/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
 import logo from '../assets/logo.svg';
 import '../stylesheets/App.css';
-import searchBar from './search'
+import searchBar from './search.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <searchBar />
-      </div>
     );
   }
 }
