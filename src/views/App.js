5071b6fe953efa5b8b0d2eb9792182ddd825c2cd
CoffeeYes <culinecro@gmail.com>
2018-04-25 20:43:46 +0200

    added non-functional searchbar component

--- src/views/App.js
+++ src/views/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import logo from '../assets/logo.svg';
 import '../stylesheets/App.css';
 import Test from './test'
+import SearchBar from './searchbar'
 
 class App extends Component {
   render() {
     return(
-      <Test />
+      <div>
+        <Test />
+        <SearchBar />
+      </div>
     )
   }
 }
