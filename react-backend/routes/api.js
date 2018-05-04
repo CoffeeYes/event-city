4a156f3ae6519848f2d49f036e1fed1c84aa266b
CoffeeYes <culinecro@gmail.com>
2018-05-04 15:44:18 +0200

    setup mongo, currently throws errors because of query feedback

--- react-backend/routes/api.js
+++ react-backend/routes/api.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var mClient = require('mongodb').MongoClient;
+var connect = require('../bin/connect.js');
 
 /* GET users listing. */
 router.get('/test', function(req, res, next) {
@@ -8,7 +10,14 @@ router.get('/test', function(req, res, next) {
 
 router.get('/cities',function(req,res,next) {
   var query = req.query
-  res.send({query_text: query})
+  res.send({query_text  : query})
+  mClient.connect(connect.mongo.url,function(error,database) {
+    if(error)throw error;
+    database.collection('cities').find({id : query}).toArray(function(error,data) {
+      if(error)throw error;
+      console.log(data)
+    })
+  })
 })
 
 module.exports = router;
