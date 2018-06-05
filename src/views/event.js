1ab41890f08a382499f2b505a30d51d2d0081dc2
CoffeeYes <culinecro@gmail.com>
2018-06-05 09:34:54 +0200

    added very basic styles to event page

--- src/views/event.js
+++ src/views/event.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class Event extends Component {
   render() {
     return(
-      <div className="eventcontainer">
-        <p>{this.props.data.name}</p>
-        <p>{this.props.data.going}</p>
+      <div className="event_data_container">
+        <h2>{this.props.data.name}</h2>
+        <p>{this.props.data.going} going</p>
         <p>{this.props.data.time}</p>
       </div>
     )
