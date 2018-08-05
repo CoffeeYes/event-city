80fd3b6771082202c0ca0cb0759210ce3e3cd5f1
CoffeeYes <culinecro@gmail.com>
2018-08-05 15:32:38 +0200

    fixed ID-coding for adding events

--- react-backend/routes/add-event.js
+++ react-backend/routes/add-event.js
@@ -11,23 +11,31 @@ router.post('/',function(req,res,next) {
     if(error)throw error;
     var database = client.db('pinterest-clone');
 
-    var data_user = {
-      date : req.body.date,
-      location : req.body.location,
-      time : req.body.time,
-      title : req.body.title,
-      city: req.body.city_specific
-    }
-    var data_city = {
-      date : req.body.date,
-      location : req.body.location,
-      time : req.body.time,
-      title : req.body.title,
-      user : req.body.user
-    }
+    database.collection("codes").find({_id : Oid("5b66f809e7179a4c61391c03")}).toArray(function(error,data) {
+      if(error)throw error;
+      var current_code = data[0]["event-code"];
 
-    database.collection('user-data').update({username : req.body.user},{$push : {events : data_user}})
-    database.collection('cities').update({city : req.body.city_specific},{$push : {events : data_city}})
+      database.collection("codes").update({_id : Oid("5b66f809e7179a4c61391c03")},{$inc : {"event-code" : 1}})
+
+      var data_user = {
+        date : req.body.date,
+        location : req.body.location,
+        time : req.body.time,
+        title : req.body.title,
+        city: req.body.city_specific
+      }
+      var data_city = {
+        date : req.body.date,
+        location : req.body.location,
+        time : req.body.time,
+        title : req.body.title,
+        user : req.body.user,
+        code : current_code
+      }
+
+      database.collection('user-data').update({username : req.body.user},{$push : {events : data_user}})
+      database.collection('cities').update({city : req.body.city_specific},{$push : {events : data_city}})
+    })
   })
 })
 
