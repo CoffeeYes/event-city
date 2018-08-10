7c995f952bb8ff583b94172cb04d9247a2a3348b
CoffeeYes <culinecro@gmail.com>
2018-08-10 18:43:26 +0200

    updated event render to frontend

--- react-backend/routes/event.js
+++ react-backend/routes/event.js
@@ -11,10 +11,10 @@ router.get('/', function(req, res, next) {
       if(error) throw error;
       var database = client.db(connect.mongo.db_name);
       database.collection('cities').find({events : {$elemMatch : {code : query}}}).toArray(function(error,data) {
-        console.log(data)
         var result = data[0].events;
         for(var item in result) {
           if (result[item].code == query) {
+            console.log(result[item])
             res.send(result[item])
           }
         }
--- src/views/event.js
+++ src/views/event.js
@@ -4,9 +4,10 @@ class Event extends Component {
   render() {
     return(
       <div className="event_data_container">
-        <h2>{this.props.data.name}</h2>
+        <h2>{this.props.data.title} - {this.props.data.location}</h2>
+        <p>{this.props.data.time} - {this.props.data.date}</p>
         <p>{this.props.data.going} going</p>
-        <p>{this.props.data.time}</p>
+        <p>Hosted By : {this.props.data.user}</p>
       </div>
     )
   }
