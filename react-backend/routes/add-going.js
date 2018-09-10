1148ff3ba3c9484c84bbd2dacb1be3d0615d9356
CoffeeYes <culinecro@gmail.com>
2018-09-10 17:40:38 +0200

    added "already going" check to "im going" functionality

--- react-backend/routes/add-going.js
+++ react-backend/routes/add-going.js
@@ -8,7 +8,12 @@ router.post('/',function(req,res,next) {
   mClient.connect(connect.mongo.url,function(error,client) {
     var id = req.body.id
     var database = client.db('pinterest-clone');
-    database.collection('user-data').update({username : req.body.user},{$push : {going : id}})
+    database.collection('user-data').find({username : req.body.user}).toArray(function(error,data) {
+      if(error)throw error;
+      if(data[0].going.indexOf(req.body.id) == -1) {
+        database.collection('user-data').update({username : req.body.user},{$push : {going : id}})
+      }
+    })
   })
 })
 
