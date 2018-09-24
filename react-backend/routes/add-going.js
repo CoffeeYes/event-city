c1a88eac4d224e9e6abc278ff6d733d9ea5aa19b
CoffeeYes <culinecro@gmail.com>
2018-09-24 18:56:11 +0200

    added repeat prevention on going

--- react-backend/routes/add-going.js
+++ react-backend/routes/add-going.js
@@ -8,9 +8,9 @@ router.post('/',function(req,res,next) {
   mClient.connect(connect.mongo.url,function(error,client) {
     var id = req.body.id
     var database = client.db('pinterest-clone');
-    database.collection('user-data').find({username : req.body.user}).toArray(function(error,data) {
+    database.collection('events').find({code : String(req.body.id)}).toArray(function(error,data) {
       if(error)throw error;
-      if(data[0].going.indexOf(req.body.id) == -1) {
+      if(data[0].going.indexOf(req.body.user) == -1) {
         database.collection('events').update({code : req.body.id},{$push : {going : req.body.user}});
         database.collection('events').update({code : req.body.id},{$inc : {going_count : 1}});
         database.collection('user_data').update({username : req.body.user},{$push : {going : req.body.id}})
