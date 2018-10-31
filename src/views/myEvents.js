5ada54fcb48ee150a4e03c5f0fa285be55358c2f
CoffeeYes <culinecro@gmail.com>
2018-10-31 16:45:55 +0100

    added render of myevents prop array

--- src/views/myEvents.js
+++ src/views/myEvents.js
@@ -4,7 +4,21 @@ import {Link} from 'react-router-dom';
   class myEvents extends Component {
   render() {
     return(
-      <p>my events</p>
+      <div className="event-container">
+        <ul>
+          {this.props.events.map((item,index) => {
+            return (
+                    <div className="event" key={index}>
+                      <h2>{item.title} - {item.location}</h2>
+                      <p>{item.time}</p>
+                      <p>{item.going_count} going</p>
+                      <p>{item.date}</p>
+                      <p>Hosted By : {item.user}</p>
+                    </div>
+            )
+          })}
+        </ul>
+      </div>
     )
   }
   }
